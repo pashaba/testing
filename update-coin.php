7e2f706fbd1b979bbc29f00fc560408c2f57f57f
PashaDev <kimtha293@gmail.com>
2026-06-18 14:16:08 +0700

    Update update-coin.php

--- update-coin.php
+++ update-coin.php
@@ -1,14 +1,7 @@
 <?php
 session_start();
-
 $amount = intval($_GET['amount'] ?? 0);
-
-if (!isset($_SESSION['user_google_id'])) {
-    echo json_encode(['success' => false, 'message' => 'Not logged in']);
-    exit;
-}
-
+if (!isset($_SESSION['user_google_id'])) { echo json_encode(['success' => false]); exit; }
 $_SESSION['user_coins'] = max(0, ($_SESSION['user_coins'] ?? 0) + $amount);
-
 echo json_encode(['success' => true, 'coins' => $_SESSION['user_coins']]);
 ?>
