e6a6bc7fa755920d24ec5aeff7964b4db1b686ed
PashaDev <kimtha293@gmail.com>
2026-06-18 13:17:37 +0700

    Add coin update functionality for logged-in users

---
+++ update-coin.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$amount = intval($_GET['amount'] ?? 0);
+
+if (!isset($_SESSION['user_google_id'])) {
+    echo json_encode(['success' => false, 'message' => 'Not logged in']);
+    exit;
+}
+
+$_SESSION['user_coins'] = ($_SESSION['user_coins'] ?? 0) + $amount;
+
+echo json_encode(['success' => true, 'coins' => $_SESSION['user_coins']]);
+?>
